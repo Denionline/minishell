069797ed4875acde9bc46a61afd2a35d94b8507d
Denionline <danielximenes164@gmail.com>
2025-09-02 17:43:28 +0100

    feat: Add the malloc and implement end function in case of error

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -1,17 +1,24 @@
 
 #include "minishell.h"
 
+// static t_cmd	get_command(t_head *head)
+// {
+// 	(void)head;
+// }
+
 void	parse(t_head *head, char *prompt)
 {
-	(void)head;
 	char	**values;
 	int		i;
 
 	values = ft_split(prompt, ' ');
+	head->cmds = malloc(sizeof(t_cmd) * get_array_len(values));
+	if (!head->cmds)
+		end(head, errno, "head->cmds");
 	i = 0;
 	while (values[i])
 	{
-		ft_putendl_fd(values[i], 1);
+		// get_command(head);
 		i++;
 	}
 }
