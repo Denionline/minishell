474ea4db9c1f6cdd0368ef7e6b54011d62645ebe
Denionline <danielximenes164@gmail.com>
2025-09-02 17:42:52 +0100

    feat: Add the the end of the program on word "exit"

--- src/minishell.c
+++ src/minishell.c
@@ -8,6 +8,9 @@ int	minishell(t_head *head)
 	while (1)
 	{
 		prompt = readline("\001\033[1;92m\002minishell\001\033[1;94m\002> \001\033[0;39m\002");
+		if (!ft_strncmp("exit", prompt, 5))
+			break ;
 		parse(head, prompt);
 	}
+	return (0);
 }
