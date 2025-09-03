cedf1b37e0ddc165254ee42df03a58703540b506
Denionline <danielximenes164@gmail.com>
2025-09-03 14:23:14 +0100

    fix: Fixed the count on array_len to count just when there is a arg and a signal as next

--- src/aux/get_array_len.c
+++ src/aux/get_array_len.c
@@ -4,9 +4,15 @@
 int	get_array_len(char **array)
 {
 	int	len;
+	int	i;
 
 	len = 0;
-	while (array[len])
-		len++;
+	i = 0;
+	while (array[i])
+	{
+		if (!is_signal(array[i]) && (is_signal(array[i + 1]) || !array[i + 1]))
+			len++;
+		i++;
+	}
 	return (len);
 }
