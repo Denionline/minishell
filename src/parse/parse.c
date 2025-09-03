103f57f4d2aba071bd3bebb48c50b340761337e4
Denionline <danielximenes164@gmail.com>
2025-09-03 14:23:57 +0100

    feat: Add the first parse that works with basic input

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -1,24 +1,70 @@
 
 #include "minishell.h"
 
-// static t_cmd	get_command(t_head *head)
-// {
-// 	(void)head;
-// }
+static int	get_size_args(char **values)
+{
+	int	i;
+
+	i = 0;
+	while (values[i] && !is_signal(values[i]))
+	{
+		i++;
+	}
+	return (i);
+}
+
+static t_cmd	get_command(t_head *head, char **values, int *pos)
+{
+	t_cmd	command;
+	int		i;
+
+	command.args = malloc(sizeof(char *) * get_size_args(values) + 1);
+	if (!command.args)
+		end(head, errno, "command.args");
+	i = 0;
+	while (values[*pos] && !is_signal(values[*pos]))
+	{
+		command.args[i++] = values[*pos];
+		(*pos)++;
+	}
+	command.args[i] = NULL;
+	if (values[*pos] && is_signal(values[*pos]))
+		command.right_sig = values[(*pos)++];
+	return (command);
+}
 
 void	parse(t_head *head, char *prompt)
 {
 	char	**values;
+	int		array_len;
+	int		pos;
 	int		i;
-
+	int		j;
+	
 	values = ft_split(prompt, ' ');
-	head->cmds = malloc(sizeof(t_cmd) * get_array_len(values));
+	array_len = get_array_len(values);
+	head->cmds = malloc(sizeof(t_cmd) * (array_len + 1));
 	if (!head->cmds)
 		end(head, errno, "head->cmds");
+	pos = 0;
 	i = 0;
-	while (values[i])
+	while (i < array_len)
 	{
-		// get_command(head);
+		head->cmds[i] = get_command(head, values, &pos);
+		i++;
+	}
+	i = 0;
+	while (i < array_len)
+	{
+		printf("%dst command:", i + 1);
+		j = 0;
+		while (head->cmds[i].args[j])
+		{
+			printf(" %s", head->cmds[i].args[j]);
+			j++;
+		}
+		printf("\n");
 		i++;
 	}
 }
+// ls -l | grep foo && echo done
