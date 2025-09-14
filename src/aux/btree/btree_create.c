c2d347690721db099325ef60939cd25978315131
Denionline <danielximenes164@gmail.com>
2025-09-14 14:42:37 +0100

    feat: Update btree structure to include fd and use identifier parameter in btree_create

--- src/aux/btree/btree_create.c
+++ src/aux/btree/btree_create.c
@@ -9,7 +9,7 @@ t_btree	*btree_create(int identifier, t_cmd *cmd, t_btree *left, t_btree *right)
 	new_token = ft_calloc(1, sizeof(t_btree));
 	if (!new_token)
 		return (NULL);
-	new_token->identifier = 42;
+	new_token->identifier = identifier;
 	new_token->cmd = cmd;
 	new_token->left = left;
 	new_token->right = right;
