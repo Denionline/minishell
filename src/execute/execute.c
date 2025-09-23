a5be193aa56bdc2bcca156e2b06d0f2559ac324c
CLMartello <carolinamartello1@gmail.com>
2025-09-23 19:19:01 +0100

    execute working again

--- src/execute/execute.c
+++ src/execute/execute.c
@@ -3,33 +3,41 @@
 
 void	execute_manager(t_head *head)
 {
+	head->fd.in = dup(STDIN_FILENO);
+	head->fd.out = dup(STDOUT_FILENO);
 	head->n_cmds = count_cmds(head->root, 0);
 	head->pid = malloc(sizeof(pid_t) * head->n_cmds);
 	if (!head->pid)
 		return ;
 	head->index = 0;
 	hierarchy_btree(head, head->root);
+	dup2(head->fd.in, STDIN_FILENO);
+	dup2(head->fd.out, STDOUT_FILENO);
 }
 
 int	hierarchy_btree(t_head *head, t_btree *node)
 {
-	int	status;
+//	int	status;
 
 	if (node == NULL)
 		return (0);
 	else if (node->identifier == COMMAND)
 		execute(head, node);
-	else if (node->identifier == PIPE)
-	{	
-		if (node->left->identifier == COMMAND)
-			execute(head, node->left); //execute node->left
-		else if (node->right->identifier == COMMAND)
-			execute(head, node->right); 
-		else
-			hierarchy_btree(head, node->left);
-		free_node(node);
+	else if (node->left && node->left->identifier == PIPE)
+		hierarchy_btree(head, node->left);
+	if (node->left && node->left->identifier == COMMAND)
+	{
+		execute(head, node->left);
+		if (node->right->identifier == COMMAND)
+			execute(head, node->right);
+//		free_node(node);
 	}
-	else if (node->identifier == AND)
+	if (node->right && node->right->identifier == COMMAND)
+	{
+		execute(head, node->right);
+//		free_node(node->right);
+	}
+/*	else if (node->identifier == AND)
 	{
 		status = hierarchy_btree(head, node->left);
 		if (status == 0)
@@ -40,48 +48,36 @@ int	hierarchy_btree(t_head *head, t_btree *node)
 		status = hierarchy_btree(head, node->left);
 		if (status != 0)
 			hierarchy_btree(head, node->right);
-	}
+	}*/
 	return (head->exit_code);
 }
 
 int	execute(t_head *head, t_btree *node)
 {
-//	int	status;
-//	int	i;
+	int	status;
 
-//	i = head->index;
 	head->pid[head->index] = child_process(head, node);
-	wait_list(head);
-/*	waitpid(head->pid[head->index], &status, 0);
-	free_node(node);
-	if (head->index == (head->n_cmds -1))
+	waitpid(head->pid[head->index], &status, 0);
+	if (head->index == (head->n_cmds - 1))
 	{
 		if (WIFEXITED(status))
 			head->exit_code = WEXITSTATUS(status);
 		else if (WIFSIGNALED(status))
 			head->exit_code = WTERMSIG(status) + 128;
-	}*/
+	}
+	free_node(node);
+	head->index++;
 	return (head->exit_code);
 }
-/*
-void	execute_with_pipe(t_head *head, t_btree *node)
-{
-	head->pid[head->index] = child_process(head, node->left);
-	free_node(node->left);
-	head->pid[head->index] = child_process(head, node->right);
-	free_node(node->right);
-	free_node(node);
-//	wait_list(head);
-}*/
 
 pid_t	child_process(t_head *head, t_btree *node)
 {
 	int	fd[2];
 	pid_t	pid;
 
-	fd_organizer(head, node, fd);
 	if (pipe(fd) == -1)
 		close_fd(fd);
+	fd_organizer(head, node, fd);
 	pid = fork();
 	if (pid == -1)
 		close_fd(fd);
@@ -99,7 +95,6 @@ pid_t	child_process(t_head *head, t_btree *node)
 			dup2(fd[0], STDIN_FILENO);
 		close_fd(fd);
 	}
-	head->index++;
 	return (pid);
 }
 
