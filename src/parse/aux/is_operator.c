84a75921c45744abd39b4520fb3f2e9adbd86a95
Denionline <danielximenes164@gmail.com>
2025-09-24 16:24:02 +0100

    feat: Add the function is_arrow_operator

--- src/parse/aux/is_operator.c
+++ src/parse/aux/is_operator.c
@@ -16,6 +16,19 @@ int	operator_size(int operator)
 	return (1);
 }
 
+int	is_arrow_operator(int operator)
+{
+	if (operator == ARROW_LEFT)
+		return (TRUE);
+	if (operator == ARROW_RIGHT)
+		return (TRUE);
+	if (operator == DOUBLE_ARROW_LEFT)
+		return (TRUE);
+	if (operator == DOUBLE_ARROW_RIGHT)
+		return (TRUE);
+	return (FALSE);
+}
+
 int	is_operator(char *value)
 {
 	if (!ft_strncmp("&&", value, 2))
