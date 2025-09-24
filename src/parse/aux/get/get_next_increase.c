9825f6800756356ee331d68aba0752f95039d888
Denionline <danielximenes164@gmail.com>
2025-09-24 16:25:07 +0100

    feat: Changed the function get_next_increase to get just the the size of the operator

--- src/parse/aux/get/get_next_increase.c
+++ src/parse/aux/get/get_next_increase.c
@@ -1,23 +1,7 @@
 
 #include "minishell.h"
 
-static int	get_size_file(char *prompt, int op_size)
-{
-	int	size;
-
-	size = op_size;
-	while (ft_isspace(prompt[size]))
-		size += 1;
-	while (prompt[size])
-	{
-		if (ft_isspace(prompt[size]))
-			break ;
-		size++;
-	}
-	return (size);
-}
-
-int	get_next_increase(int operator, char *prompt)
+int	get_next_increase(int operator)
 {
 	if (operator == AND)
 		return (operator_size(operator));
@@ -26,10 +10,10 @@ int	get_next_increase(int operator, char *prompt)
 	if (operator == DOUBLE_ARROW_LEFT)
 		return (operator_size(operator));
 	if (operator == DOUBLE_ARROW_RIGHT)
-		return (get_size_file(prompt, operator_size(operator)));
+		return (operator_size(operator));
 	if (operator == ARROW_RIGHT)
-		return (get_size_file(prompt, operator_size(operator)));
+		return (operator_size(operator));
 	if (operator == ARROW_LEFT)
-		return (get_size_file(prompt, operator_size(operator)));
+		return (operator_size(operator));
 	return (1);
 }
