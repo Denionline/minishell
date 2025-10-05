e8220d9b2ce98b4b555b9ecdc5a9fe8fea0eef13
CLMartello <carolinamartello1@gmail.com>
2025-10-05 17:04:54 +0100

    workinf ft_pwd built-in

--- include/execute.h
+++ include/execute.h
@@ -14,10 +14,21 @@ void    execute_manager(t_head *head);
 void    fd_organizer(t_head *head, t_btree *node, int *fd);
 int	count_cmds(t_btree *node, int j);
 
+//free
 void	free_all(t_head *head, t_btree *node, int *fd);
 void	free_btree(t_btree *node);
 void	free_node(t_btree *node);
 void	free_db_str(char **db_str);
 void	close_fd(int *fd);
+
+//built-ins
+int	is_builtin(t_btree *node);
+void    call_builtin(t_btree *node);
+void	ft_pwd(t_btree *node);
+
+//signals
+void    signal_handler(void);
+void    signal_tty(void);
+void    signal_child(void);
 
 #endif
