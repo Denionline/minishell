6fe61fb9b4e5812c4c38810d24b53146df3dcb9b
denionline <danielximenes164@gmail.com>
2025-10-08 14:11:28 +0100

    feat: Updating to git

--- src/parse/aux/get/get_string_argument.c
+++ src/parse/aux/get/get_string_argument.c
@@ -1,6 +1,23 @@
 
 #include "minishell.h"
 
+static int	is_tohandle_backslash(char *c, char quote)
+{
+	if (*c != '\\' || quote == '\'')
+		return (0);
+	c++;
+	if (*c == '\"')
+		return (1);
+	if (*c == '\'')
+		return (1);
+	if (*c == '$')
+		return (1);
+	if (*c == '\\')
+		return (1);
+	if (*c == 'n')
+		return (1);
+}
+
 static void	verify_quotes(t_quotes *quotes, char c)
 {
 	if (c == '\'')
@@ -24,32 +41,45 @@ static int	is_main_quote_closed(t_quotes *quotes)
 	return (FALSE);
 }
 
-char	*get_string_argument(char *string)
+static int	get_string_argument_size(char *string)
 {
 	t_quotes	quotes;
-	char		*string_argument;
 	int			size;
-	int			i;
-
+	int			jumps;
+	
 	ft_bzero(&quotes, sizeof(quotes));
+	jumps = 0;
 	size = 0;
 	while (string[size])
 	{
+		if (is_tohandle_backslash(string + size, quotes.quote))
+			jumps += 1;
 		if (is_main_quote_closed(&quotes) && get_operator(string + size))
 			break ;
 		if (is_main_quote_closed(&quotes) && ft_isspace(string[size]))
 			break ;
 		if (string[size] == '\'' || string[size] == '\"')
 			verify_quotes(&quotes, string[size]);
-		size++;
+		size += 1;
 	}
-	string_argument = ft_calloc(size + 1, 1);
+	return (size - jumps);
+}
+
+char	*get_string_argument(char *string)
+{
+	
+	const int	string_size = get_string_argument_size(string);
+	char		*string_argument;
+	int			i;
+	
+	string_argument = ft_calloc(string_size + 1, 1);
 	if (!string_argument)
 		return (NULL);
 	i = 0;
-	while (i < size)
+	while (i < string_size)
 	{
-		string_argument[i] = string[i];
+		if (!is_tohandle_backslash(string + i, string[0]))
+			string_argument[i] = string[i];
 		i++;
 	}
 	string_argument[i] = '\0';
