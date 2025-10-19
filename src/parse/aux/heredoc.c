5ce9eb67fd7f620242aa55e3e4423b1872c3d364
Denionline <danielximenes164@gmail.com>
2025-10-19 11:31:06 +0100

    fix: Correctly quote strings in heredoc function

--- src/parse/aux/heredoc.c
+++ src/parse/aux/heredoc.c
@@ -6,11 +6,23 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 10:58:28 by dximenes          #+#    #+#             */
-/*   Updated: 2025/10/19 10:47:17 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/10/19 11:27:11 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static char	*quote_string(char *string)
+{
+	char	*string_left_quoted;
+	char	*string_right_quoted;
+
+	string_left_quoted = ft_strjoin("\"", string);
+	free(string);
+	string_right_quoted = ft_strjoin(string_right_quoted, "\"");
+	free(string_left_quoted);
+	return (string_right_quoted);
+}
 
 t_file	heredoc(char *eof, char **envp)
 {
@@ -28,6 +40,7 @@ t_file	heredoc(char *eof, char **envp)
 		line = readline("> ");
 		if ((!ft_strncmp(eof, line, ft_strlen(eof))) || !line)
 			break ;
+		line = quote_string(line);
 		line = string_argument(line, envp, NULL, TRUE);
 		ft_putstr_fd(line, heredoc_file.fd);
 		ft_putstr_fd("\n", heredoc_file.fd);
