523920f7391c8dfb2f9adbea63018aef4f26d82a
CLMartello <carolinamartello1@gmail.com>
2025-10-26 18:16:30 +0000

    feature: initial handle of command not found

--- src/error/error.c
+++ src/error/error.c
@@ -23,9 +23,23 @@ void	ft_error_file(t_head *head, t_btree *node, int *fd, int error)
 	exit(1);
 }
 
+void	ft_error_command(t_head *head, t_btree *node, int *fd)
+{
+	free(head->pid);
+	if (head->pipe.flag == 1)
+		close_fd(fd);
+	dup2(head->files.in.fd, STDIN_FILENO);
+        dup2(head->files.out.fd, STDOUT_FILENO);
+	close_all_fds(head, node, 0);
+	head->exit_code = 127;
+	exit(1);
+}
+
 void	ft_error(t_head *head, t_btree *node, int *fd, int error)
 {
 	if (error == 0 || error == 1)
 		ft_error_file(head, node, fd, error);
+	else if (error == 2)
+		ft_error_command(head, node, fd);
 }
 
