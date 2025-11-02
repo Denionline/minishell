d2e226009e7b96216e7541f1ba64cf26d6f0874e
CLMartello <carolinamartello1@gmail.com>
2025-11-02 15:54:53 +0000

    fix: valgrind error on builtins

--- src/builtin/aux/builtin.c
+++ src/builtin/aux/builtin.c
@@ -27,12 +27,11 @@ void	call_builtin(t_head *head, t_btree *node)
 	if (!ft_strncmp("pwd", command, ft_strlen("pwd")))
 		ft_pwd(node);
 	if (!ft_strncmp("env", command, ft_strlen("env")))
-		exit(ft_env(head->env.vars));
+		ft_env(head->env.vars);
 	if (!ft_strncmp("echo", command, ft_strlen("echo")))
-		exit(ft_echo(node->cmd));
+		ft_echo(node->cmd);
 	free_node(node);
-//	free(head->pid);
-	head->exit_code = 0;
+	free_head(head);
 	exit(0);
 }
 
