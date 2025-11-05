b7840abfec4bbf72abbbf530df19896d59ea6791
denionline <danielximenes164@gmail.com>
2025-11-05 11:48:14 +0000

    fix: Fixed the print

--- src/aux/print.c
+++ src/aux/print.c
@@ -15,14 +15,20 @@ static void	print_operator(t_btree *node)
 
 static void	print_command(t_btree *node)
 {
-	printf("path: %s and args:", node->cmd->path);
-	for (int i = 0; node->cmd->args[i]; i++) {
-		printf(" arg[%d]: %s", i, node->cmd->args[i]);
+	if (node->identifier == EMPTY)
+		printf("(empty node)");
+	if (node->cmd->path) {
+		printf("path: %s and args: ", node->cmd->path);
+	}
+	if (node->cmd->args) {
+		for (int i = 0; node->cmd->args[i]; i++) {
+			printf("arg[%d]: %s ", i, node->cmd->args[i]);
+		}
 	}
 	if (node->files.out.exists)
-		printf(" (out) associated [%s]", node->files.out.name);
+		printf("(out)[%s]", node->files.out.name);
 	if (node->files.in.exists)
-		printf(" (in) associated [%s]", node->files.in.name);
+		printf("(in)[%s]", node->files.in.name);
 	printf("\n");
 }
 
@@ -32,7 +38,7 @@ void	print(t_btree *token)
 		return ;
 	if (token->left)
 		print(token->left);
-	if (token->identifier == COMMAND)
+	if (token->identifier == COMMAND || token->identifier == EMPTY)
 		print_command(token);
 	else
 		print_operator(token);
