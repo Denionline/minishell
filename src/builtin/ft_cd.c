840499945dd9d8e25ebde941a07f463ded3bb9aa
CLMartello <carolinamartello1@gmail.com>
2025-11-07 14:22:55 +0000

    fix: ft_cd error message

--- src/builtin/ft_cd.c
+++ src/builtin/ft_cd.c
@@ -5,7 +5,7 @@ int	ft_cd(t_head *head, t_btree *node)
 {
 	if (chdir(node->cmd->args[1]) == -1)
 	{
-		write(2, "minishell: No such file or directory: ", 38);
+		write(2, "minishell: No such file or directory: cd: ", 42);
 		ft_putendl_fd(node->cmd->args[1], 2);
 		head->exit_code = 1;
 	}
