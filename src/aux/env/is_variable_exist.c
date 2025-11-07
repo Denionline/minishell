2f46025a896ae11c7d5213ba17bee74b0cd425fb
Denionline <danielximenes164@gmail.com>
2025-11-07 11:32:38 +0000

    fix: Improve variable handling in ft_export and is_variable_exist functions

--- src/aux/env/is_variable_exist.c
+++ src/aux/env/is_variable_exist.c
@@ -6,12 +6,14 @@ int	is_variable_exist(char *variable, char **vars)
 	const int	lvariable = ft_strlen(variable);
 	int		pos;
 	
-	pos = 0;
-	while (vars[pos])
+	pos = -1;
+	while (vars[++pos])
 	{
 		if (!ft_strncmp(variable, vars[pos], lvariable))
-			return (pos);
-		pos++;
+		{
+			if (vars[pos][lvariable] == '=' || vars[pos][lvariable] == '\0')
+				return (pos);
+		}
 	}
 	return (-1);
 }
