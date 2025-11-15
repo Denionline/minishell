6be6b680c0a4c26efe2bd4a4fc8cc713548daf37
CLMartello <carolinamartello1@gmail.com>
2025-11-15 11:30:46 +0000

    fix: builtin call with size of command

--- src/builtin/aux/builtin.c
+++ src/builtin/aux/builtin.c
@@ -2,19 +2,22 @@
 
 int	is_builtin(char *command)
 {
-	if (!ft_strncmp("exit", command, ft_strlen("exit")))
+	int	size;
+
+	size = ft_strlen(command);
+	if (!ft_strncmp("exit", command, size))
 		return (TRUE);
-	if (!ft_strncmp("pwd", command, ft_strlen("pwd")))
+	if (!ft_strncmp("pwd", command, size))
 		return (TRUE);
-	if (!ft_strncmp("cd", command, ft_strlen("cd")))
+	if (!ft_strncmp("cd", command, size))
 		return (TRUE);
-	if (!ft_strncmp("env", command, ft_strlen("env")))
+	if (!ft_strncmp("env", command, size))
 		return (TRUE);
-	if (!ft_strncmp("export", command, ft_strlen("export")))
+	if (!ft_strncmp("export", command, size))
 		return (TRUE);
-	if (!ft_strncmp("unset", command, ft_strlen("unset")))
+	if (!ft_strncmp("unset", command, size))
 		return (TRUE);
-	if (!ft_strncmp("echo", command, ft_strlen("echo")))
+	if (!ft_strncmp("echo", command, size))
 		return (TRUE);
 	return (FALSE);
 }
@@ -22,12 +25,14 @@ int	is_builtin(char *command)
 void	call_builtin(t_head *head, t_btree *node)
 {
 	const char	*command = node->cmd->args[0];
+	int			size;
 
-	if (!ft_strncmp("pwd", command, ft_strlen("pwd")))
+	size = ft_strlen(command);
+	if (!ft_strncmp("pwd", command, size))
 		ft_pwd();
-	if (!ft_strncmp("env", command, ft_strlen("env")))
+	if (!ft_strncmp("env", command, size))
 		ft_env(head->env.vars, FALSE);
-	if (!ft_strncmp("echo", command, ft_strlen("echo")))
+	if (!ft_strncmp("echo", command, size))
 		ft_echo(node->cmd);
 	is_parent_builtin(head, node);
 	free_node(node);
@@ -39,18 +44,20 @@ int	is_parent_builtin(t_head *head, t_btree *node)
 {
 	char	*command;
 	int		ok;
+	int		size;
 
 	ok = 1;
 	if (node->cmd->args == NULL)
 		return (0);
 	command = node->cmd->args[0];
-	if (!ft_strncmp("exit", command, ft_strlen("exit")) && head->n_cmds == 1)
+	size = ft_strlen(command);
+	if (!ft_strncmp("exit", command, size) && head->n_cmds == 1)
 		ok = ft_exit(head, node);
-	if (!ft_strncmp("cd", command, ft_strlen("cd")))
+	if (!ft_strncmp("cd", command, size))
 		ok = ft_cd(head, node);
-	if (!ft_strncmp("export", command, ft_strlen("export")))
+	if (!ft_strncmp("export", command, size))
 		ok = ft_export(head, node, NULL);
-	if (!ft_strncmp("unset", command, ft_strlen("unset")))
+	if (!ft_strncmp("unset", command, size))
 		ok = ft_unset(node->cmd, &head->env);
 	head->is_parent = ok;
 	return (ok);
