221a57eb70bf09ccafcbcb9de5beb0b480504a8c
CLMartello <carolinamartello1@gmail.com>
2025-11-15 19:45:44 +0000

    fix: details

--- src/parse/aux/heredoc.c
+++ src/parse/aux/heredoc.c
@@ -34,6 +34,7 @@ t_file	heredoc(t_head *head, char *eof)
 	save_stdin = dup(STDIN_FILENO);
 	get_lines(head, &heredoc_file, eof);
 	dup2(save_stdin, STDIN_FILENO);
+	close(save_stdin);
 	close(heredoc_file.fd);
 	if (define_exit_code(0, FALSE) == 130)
 		head->to_stop = TRUE;
