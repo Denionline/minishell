2748b400c4a78d152dbb7dba17a515818bd1076a
Denionline <danielximenes164@gmail.com>
2025-11-18 18:19:29 +0000

    change: Replace the function ft_strncmp to is_strmatch to get a better syntaxe.

--- src/builtin/aux/builtin.c
+++ src/builtin/aux/builtin.c
@@ -2,32 +2,33 @@
 
 int	is_builtin(char *command)
 {
-	if (!ft_strncmp("exit", command, 5))
+	if (is_strmatch(command, "exit"))
 		return (TRUE);
-	if (!ft_strncmp("pwd", command, 4))
+	if (is_strmatch(command, "pwd"))
 		return (TRUE);
-	if (!ft_strncmp("cd", command, 3))
+	if (is_strmatch(command, "cd"))
 		return (TRUE);
-	if (!ft_strncmp("env", command, 4))
+	if (is_strmatch(command, "env"))
 		return (TRUE);
-	if (!ft_strncmp("export", command, 7))
+	if (is_strmatch(command, "export"))
 		return (TRUE);
-	if (!ft_strncmp("unset", command, 6))
+	if (is_strmatch(command, "unset"))
 		return (TRUE);
-	if (!ft_strncmp("echo", command, 5))
+	if (is_strmatch(command, "echo"))
 		return (TRUE);
 	return (FALSE);
 }
 
 void	call_builtin(t_head *head, t_btree *node)
 {
-	const char	*command = node->cmd->args[0];
+	char	*command;
 
-	if (!ft_strncmp("pwd", command, 4))
+	command = node->cmd->args[0];
+	if (is_strmatch(command, "pwd"))
 		ft_pwd();
-	if (!ft_strncmp("env", command, 4))
+	if (is_strmatch(command, "env"))
 		ft_env(head->env.vars, FALSE);
-	if (!ft_strncmp("echo", command, 5))
+	if (is_strmatch(command, "echo"))
 		ft_echo(node->cmd);
 	is_parent_builtin(head, node);
 	free_btree(head->root);
@@ -47,13 +48,13 @@ int	is_parent_builtin(t_head *head, t_btree *node)
 		return (define_exit_code(0, TRUE));
 	}
 	command = node->cmd->args[0];
-	if (!ft_strncmp("exit", command, 5) && head->n_cmds == 1)
+	if (is_strmatch(command, "exit") && head->n_cmds == 1)
 		ok = ft_exit(head, node);
-	if (!ft_strncmp("cd", command, 3))
+	if (is_strmatch(command, "cd"))
 		ok = ft_cd(head, node);
-	if (!ft_strncmp("export", command, 7))
+	if (is_strmatch(command, "export"))
 		ok = ft_export(head, node, NULL);
-	if (!ft_strncmp("unset", command, 6))
+	if (is_strmatch(command, "unset"))
 		ok = ft_unset(node->cmd, &head->env);
 	head->is_parent = ok;
 	return (ok);
--- src/builtin/ft_export.c
+++ src/builtin/ft_export.c
@@ -36,7 +36,13 @@ static int	handle_variable(t_head *head, char *complete_var)
 			ft_substr(complete_var, 0, lvar), lvar, complete_var, &head->env
 		);
 	else
-		ft_error(head, (t_error){.id = ERR_EXPORT, .string = complete_var});
+		ft_error(head,
+			(t_error){
+				.id = ERR_EXPORT,
+				.string = complete_var,
+				.msg.where = "export"
+			}
+		);
 	return (0);
 }
 
