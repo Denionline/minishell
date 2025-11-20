e1c1751e1338ea7ae8f1f047f511c86103a4bdbc
Denionline <danielximenes164@gmail.com>
2025-11-20 14:20:15 +0000

    fix: correct quote verification logic in is_quotes_valid function

--- src/aux/is_quotes_valid.c
+++ src/aux/is_quotes_valid.c
@@ -6,7 +6,7 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 14:15:56 by dximenes          #+#    #+#             */
-/*   Updated: 2025/11/20 14:15:57 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/11/20 14:20:07 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,16 @@ int	is_quotes_valid(char *s)
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] == '\'' || s[i] == '\"')
-			verify_quotes(&quotes, s[i], FALSE);
+		if (is_tohandle_backslash(s + i, quotes.quote))
+			i++;
+		else
+		{
+			if (s[i] == '\'' || s[i] == '\"')
+				verify_quotes(&quotes, s[i], FALSE);
+		}
 		i++;
 	}
-	if ((quotes.s % 2) == 0 && (quotes.d % 2) == 0)
+	if ((quotes.s % 2) == 0 && (quotes.d% 2) == 0)
 		return (TRUE);
 	return (FALSE);
 }
