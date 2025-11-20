d51bc5fee8d7e0cdeca599fbd05173f4f13e5349
Denionline <danielximenes164@gmail.com>
2025-11-20 10:31:27 +0000

    refactor: Simplify prompt_prefix function by introducing putstrbuff for string copying

--- src/aux/prompt_prefix.c
+++ src/aux/prompt_prefix.c
@@ -8,32 +8,32 @@ static char	*get_arrow_color(int exit_code)
 		return (LGREEN " ➜ ");
 }
 
+static void	putstrbuff(const char *str, char buffer[9999], int *pos)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		buffer[(*pos)++] = str[i++];
+}
+
 void	prompt_prefix(char buffer[9999])
 {
-	const char	*s1 = (BOLD LGREEN "minishell");
 	const char	*arrow = (get_arrow_color(define_exit_code(0, FALSE)));
-	const char	*s2 = (YELLOW "(" LMAGENTA);
-	const char	*s3 = (YELLOW ") > " RESET);
 	char		cwd[999];
 	int			pos;
 	int			i;
 
 	getcwd(cwd, sizeof(cwd));
 	pos = 0;
-	i = 0;
-	while (s1[i])
-		buffer[pos++] = s1[i++];
+	putstrbuff(BOLD LGREEN "minishell", buffer, &pos);
 	i = 0;
 	while (arrow[i])
 		buffer[pos++] = arrow[i++];
-	i = 0;
-	while (s2[i])
-		buffer[pos++] = s2[i++];
+	putstrbuff(YELLOW "(" LMAGENTA, buffer, &pos);
 	i = 0;
 	while (cwd[i])
 		buffer[pos++] = cwd[i++];
-	i = 0;
-	while (s3[i])
-		buffer[pos++] = s3[i++];
+	putstrbuff(YELLOW ") > " RESET, buffer, &pos);
 	buffer[pos] = '\0';
 }
